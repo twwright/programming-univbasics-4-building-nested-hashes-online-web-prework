b8e1f39ea3c30b78e302b273643b2e75fda3da5a
Thomas Wright <twwright@gmail.com>
2019-09-30 05:35:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,9 +33,8 @@ def monopoly_with_fourth_tier
 			:one_piece_owned => 25,
 			:two_pieces_owned => 50,
 			:three_pieces_owned => 100,
-			:four_pieces_owned => 200
-			},
-		:names => {
+			:four_pieces_owned => 200,
+			:names =>
 			}
 		}
 	}
