0e6e38973cab53ba3ec88dbf3b64bfe580c6327e
Thomas Wright <twwright@gmail.com>
2019-09-30 05:43:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
 			},
 		:names => {
-			:reading_railord => {},
+			:reading_railroad => {},
 			:pennsylvania_railroad => {},
 			:b_and_o_railroad => {},
 			:shortline => {}
