dacf979992f305b41d280fa72651aed56b8c8143
Thomas Wright <twwright@gmail.com>
2019-09-30 05:38:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,8 @@ def monopoly_with_third_tier
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
-			}
+			},
+		:names => {}
 		}
 	}
 end
@@ -35,7 +36,18 @@ def monopoly_with_fourth_tier
 			:three_pieces_owned => 100,
 			:four_pieces_owned => 200
 			},
-		:names => {}
-		}
+			:reading_railroad = > {
+				"mortgage_value" => "$100"
+				},
+			:pennsylvania_railroad => {
+				"mortgage_value" => "$200"
+				},
+			:b_and_o_railroad => {
+				"mortgage_value" => "$400"
+				},
+			:shortline=> {
+				"mortgage_value" => "$800"
+				}
+			}
 	}
 end
